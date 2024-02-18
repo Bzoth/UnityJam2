4ce05936e20408dcfe3dc225df80a155d74ea9e3
Bzoth <yunusbasturk2005@gmail.com>
2024-02-18 16:36:15 +0300

    Commit 11
    0.90

--- Assets/Scripts/GameManager/GoldManage.cs
+++ Assets/Scripts/GameManager/GoldManage.cs
@@ -78,7 +78,89 @@ public class GoldManage : MonoBehaviour
 
     public void TierChanger()
     {
+        if(speedUpgradeCost == 3)
+        {
+            speedTier.sprite = tier2;
+        }
+        else if(speedUpgradeCost == 5)
+        {
+            speedTier.sprite = tier3;
+        }
+        else if(speedUpgradeCost == 7)
+        {
+            speedTier.sprite = tier4;
+        }
+        else if(speedUpgradeCost == 9)
+        {
+            speedTier.sprite = tier5;
+        }
+        else if(speedUpgradeCost == 11)
+        {
+            speedTier.sprite = tier6;
+        }
 
+        if(damageUpgradeCost == 3)
+        {
+            damageTier.sprite = tier2;
+        }
+        else if(damageUpgradeCost == 5)
+        {
+            damageTier.sprite = tier3;
+        }
+        else if(damageUpgradeCost == 7)
+        {
+            damageTier.sprite = tier4;
+        }
+        else if(damageUpgradeCost == 9)
+        {
+            damageTier.sprite = tier5;
+        }
+        else if(damageUpgradeCost == 11)
+        {
+            damageTier.sprite = tier6;
+        }
+
+        if(attackSpeedUpgradeCost == 3)
+        {
+            attackSpeedTier.sprite = tier2;
+        }
+        else if(attackSpeedUpgradeCost == 5)
+        {
+            attackSpeedTier.sprite = tier3;
+        }
+        else if(attackSpeedUpgradeCost == 7)
+        {
+            attackSpeedTier.sprite = tier4;
+        }
+        else if(attackSpeedUpgradeCost == 9)
+        {
+            attackSpeedTier.sprite = tier5;
+        }
+        else if(attackSpeedUpgradeCost == 11)
+        {
+            attackSpeedTier.sprite = tier6;
+        }
+
+        if(HealthUpgradeCost == 3)
+        {
+            healthTier.sprite = tier2;
+        }
+        else if(HealthUpgradeCost == 5)
+        {
+            healthTier.sprite = tier3;
+        }
+        else if(HealthUpgradeCost == 7)
+        {
+            healthTier.sprite = tier4;
+        }
+        else if(HealthUpgradeCost == 9)
+        {
+            healthTier.sprite = tier5;
+        }
+        else if(HealthUpgradeCost == 11)
+        {
+            healthTier.sprite = tier6;
+        }
     }
     
 }
